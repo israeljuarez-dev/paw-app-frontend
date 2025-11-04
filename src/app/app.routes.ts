1261c247b8b4415a4220f166e27627263eea28ba
Israel <israel.bastion123@gmail.com>
2025-11-03 23:53:05 -0500

    view finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ReservaComponent } from './pages/reserva/reserva.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path:'reservas',
+    component: ReservaComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signin',
+    component: RegisterComponent
+  }
+];
